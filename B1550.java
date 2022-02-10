c3261c6c3f2cb9b9b703e7d175ddc6356d230c36
LeeHuiJu <leemoon63@gmail.com>
2022-02-10 22:23:57 +0900

    Integer.parseInt()

---
+++ B1550.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class B1550 {
+	 public static void main(String[] args) {
+	      Scanner sc=new Scanner(System.in);
+	      String n=sc.next();
+	      System.out.println(Integer.parseInt(n,16));     
+     }
+}
