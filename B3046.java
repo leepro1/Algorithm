ee02952bd3ae7da4057e9ab70096c24c39ecf9c1
LeeHuiJu <leemoon63@gmail.com>
2022-02-13 20:12:11 +0900

    avg

---
+++ B3046.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class B3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		
+		System.out.println(S*2 - R1);
+		
+		sc.close();
+	}
+}
