ba0a6ad0e5be242c10fb9377b501e7733ff9cc30
LeeHuiJu <leemoon63@gmail.com>
2022-02-13 20:10:37 +0900

    *

---
+++ B2914.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class B2914 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int I = sc.nextInt();
+		
+		System.out.println(A*I-(A-1));
+		
+		sc.close();
+	}
+}
