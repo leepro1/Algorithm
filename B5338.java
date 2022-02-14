bd800e10f3a2db22cb713cb6eea707960558a064
LeeHuiJu <leemoon63@gmail.com>
2022-02-14 18:43:14 +0900

    *

---
+++ B5338.java
@@ -0,0 +1,11 @@
+
+public class B5338 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-;;-._|\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-''-._|");
+		
+	}
+}
---
+++ B5339.java
@@ -0,0 +1,15 @@
+
+public class B5339 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\\n" + 
+				"    ( oo|\n" + 
+				"    _\\=/_\n" + 
+				"   /  _  \\\n" + 
+				"  //|/.\\|\\\\\n" + 
+				" ||  \\ /  ||\n" + 
+				"============\n" + 
+				"|          |\n" + 
+				"|          |\n" + 
+				"|          |");
+	}
+}
