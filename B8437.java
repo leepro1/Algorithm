29de3d37cb44b62efe943b8cbcc53985e9d414a4
LeeHuiJu <leemoon63@gmail.com>
2022-02-17 23:23:39 +0900

    Julka2

--- B8437.java
+++ B8437.java
@@ -5,15 +5,16 @@ public class B8437 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		BigInteger totalApple = sc.nextBigInteger();
-		BigInteger difApple = sc.nextBigInteger();
-		BigInteger two = new BigInteger("2");
+		BigInteger diffApple = sc.nextBigInteger();
+		BigInteger two = new BigInteger("2"); //BigInteger의 인자값은 문자열
 		
-		BigInteger k = (totalApple.subtract(difApple)).divide(two).add(difApple); //(total-diff)/2+diff
-	    BigInteger n = (totalApple.subtract(difApple)).divide(two); //(total-diff)/2
-	        
-	    System.out.println(k);
+		//문자열이기에 사칙연산 x - 내부메서드 사용
+		BigInteger k = (totalApple.subtract(diffApple)).divide(two).add(diffApple); //(total-diff)/2+diff
+		BigInteger n = (totalApple.subtract(diffApple)).divide(two); //(total-diff)/2
+		
+		System.out.println(k);
 	    System.out.println(n);
 		
-		sc.close();
+	    sc.close();
 	}
 }
