804985d9aa1234df08c10aa5a503a4eaf9e86dd4
LeeHuiJu <leemoon63@gmail.com>
2022-02-19 14:34:29 +0900

    *

---
+++ B15894.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		long upSide = n;
+		long downSide = n;
+		long bothSide = n*2;
+		
+		System.out.println(upSide+downSide+bothSide);
+		
+		sc.close();
+	}
+}
