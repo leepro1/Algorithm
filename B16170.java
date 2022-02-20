b2f145aa9abdc39989a89d173a818beadd77b49b
LeeHuiJu <leemoon63@gmail.com>
2022-02-20 21:57:16 +0900

    java.time package

---
+++ B16170.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate; //after java8 
+
+public class B16170 {
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		
+		System.out.println(now.getYear());
+		System.out.println(now.getMonth().getValue());
+		System.out.println(now.getDayOfMonth());
+	}
+}
