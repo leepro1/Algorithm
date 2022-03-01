16c552ee6dc55761c1ba677be2e6609df027ea2c
LeeHuiJu <leemoon63@gmail.com>
2022-03-01 22:51:46 +0900

    달팽이 시간초과

---
+++ B2869.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class B2869 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //객체 생성
+		StringTokenizer st = new StringTokenizer(br.readLine()," "); //사용자 입력한줄을 띄어쓰기를 기준으로 나눠 받습니다.
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		int nowHeight=0;
+		int day=1;
+		
+		while(true) {
+			nowHeight+=A;
+			if(nowHeight>=V) {
+				break;
+			}
+			nowHeight-=B;
+			day++;
+		}
+		System.out.println(day);
+	}
+}
