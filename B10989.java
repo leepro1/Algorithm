f84135addab12cd7b9803db54928b918b3943d64
LeeHuiJu <leemoon63@gmail.com>
2022-03-08 21:26:26 +0900

    오름차순

---
+++ B10989.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class B10989 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb=new StringBuffer();
+		
+		int N=Integer.parseInt(br.readLine());
+		int[] arr=new int[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0;i<N;i++) {
+			sb.append(arr[i]+"\n");
+		}
+		System.out.println(sb);
+	}
+}
