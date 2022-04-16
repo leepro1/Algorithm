8db724911781d24bf47c74323da1f95168fe046a
LeeHuiJu <leemoon63@gmail.com>
2022-04-17 04:31:42 +0900

    요세푸스 수열

--- baekjoon/B11866.java
+++ baekjoon/B11866.java
@@ -1,46 +1,33 @@
 package baekjoon;
-//큐배우면 다시 풀기
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.ArrayList;
-
 
 public class B11866 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		
+		Queue<Integer> queue=new LinkedList<>();
+		
 		StringBuilder sb=new StringBuilder();
-		sb.append("<");
-		
-		
+		sb.append('<');
+
 		int N=sc.nextInt();
 		int K=sc.nextInt();
 		
-		int[] arr=new int[N];
+		for(int i=1;i<=N;i++)
+			queue.add(i);
 		
-		for(int i=0;i<N;i++) {
-			arr[i]=i+1;
+		while(queue.size()!=1){
+			for(int i=0;i<K-1;i++)
+				queue.add(queue.poll());
+			
+			sb.append(queue.poll()).append(", ");
 		}
+		sb.append(queue.poll()).append(">");
 		
-		int temp=K;
-		for(int i=0;i<N;i++) {
-			sb.append(Integer.toString(arr[temp-1]));
-			arr[temp-1]=0;
-			
-			temp+=K;
-			
-			if(temp>N)
-				temp=temp%N;
-			
-			while(arr[temp-1]==0) {
-				temp++;
-				if(temp>N)
-					temp=1;
-			}
-			System.out.println(sb);	
-		}
-		sb.append(">");
-		
-		
+		System.out.println(sb);
 		sc.close();
 	}
 }
