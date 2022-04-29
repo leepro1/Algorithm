c941a952b2008a45114dd19fbf14c8d5e1a775ed
LeeHuiJu <leemoon63@gmail.com>
2022-04-29 22:38:03 +0900

    일곱난쟁이

---
+++ baekjoon/B2309.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2309 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		int[] arr=new int[9];
+		int sum=0;
+		
+		for(int i=0;i<9;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+			sum+=arr[i];
+		}
+		Arrays.sort(arr);
+		
+		for(int i=0;i<8;i++) {
+			for(int j=i+1;j<9;j++) {
+				if(sum-arr[i]-arr[j]==100) {
+					for(int k=0;k<9;k++) {
+						if(i==k || j==k)
+							continue;
+						sb.append(arr[k]).append("\n");	
+					}
+					System.out.println(sb);
+					System.exit(0);
+				}
+			}
+		}
+		
+	}
+}
