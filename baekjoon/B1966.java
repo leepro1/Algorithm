0fe0e24cbbc2b1d57af15898899d6c531cf92e75
LeeHuiJu <leemoon63@gmail.com>
2022-05-05 23:54:25 +0900

    프린터 큐_큐순회

--- baekjoon/B1966.java
+++ baekjoon/B1966.java
@@ -4,8 +4,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -14,43 +12,41 @@ public class B1966 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb=new StringBuilder();
-		Queue<Integer> queue=new LinkedList<>();
-		
-		int T=Integer.parseInt(br.readLine());
-		int doc=0;
-		int temp;
-		int cnt=0;
+		Queue<int[]> queue=new LinkedList<>();
 		
 		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int T=Integer.parseInt(st.nextToken());
 		
 		while(T-->0) {
 			st=new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
-			ArrayList<Integer> list=new ArrayList<>();
+			int cnt=0;
 			
 			st=new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
-				temp=Integer.parseInt(st.nextToken());
-				if(i==M)
-					doc=temp;
-				list.add(temp);
-				queue.offer(temp);
+				queue.offer(new int[] {i,Integer.parseInt(st.nextToken())});
 			}
 			
-			list.sort(Comparator.reverseOrder());
-			
-			for(int i=0;;) {
-				temp=list.get(i);
-				if(temp==doc)
-					break;
-				else if(temp==queue.peek()) {
-					queue.remove();
+			while(true) {
+				int[] head=queue.remove(); //순회하기 때문에 head를 peek 이 아닌 remove
+				boolean check=true;
+				
+				for(int temp[]:queue) {
+					if(temp[1]>head[1]) {
+						check=false;
+						break;
+					}
+				}
+				
+				if(check) {
 					cnt++;
-					i++;
+					if(head[0]==M)
+						break;
 				}
 				else
-					queue.offer(queue.poll());
+					queue.offer(head);
 			}
 			sb.append(cnt).append("\n");
 			cnt=0;
