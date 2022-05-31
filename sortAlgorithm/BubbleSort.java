9d1e695add1f64dd30306f3707eeebd1fc4f7c24
LeeHuiJu <leemoon63@gmail.com>
2022-05-31 23:37:54 +0900

    bubble sort

---
+++ sortAlgorithm/BubbleSort.java
@@ -0,0 +1,33 @@
+package sortAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BubbleSort {
+	public static void bubbleSort(int[] arr,int n) {
+		for(int i=n-1;i>0;i--) {
+			for(int j=0;j<i;j++) {
+				if(arr[j]>arr[j+1])
+					swap(arr,j,j+1);
+			}
+		}
+	}
+	
+	public static void swap(int[] arr,int i,int j) {
+		int temp=arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		int[] arr=new int[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		bubbleSort(arr,N);
+	}
+}
