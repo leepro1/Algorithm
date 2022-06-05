c96acb6fa9c5f10ee387e8457c122cea87d1ebff
LeeHuiJu <leemoon63@gmail.com>
2022-06-05 14:51:09 +0900

    merge sort

--- sortAlgorithm/MergeSort.java
+++ sortAlgorithm/MergeSort.java
@@ -16,13 +16,24 @@ public class MergeSort {
 	}
 	
 	public static void merge(int[] arr, int lb, int middle, int ub) {
-		//정렬 한 뒤 병합하는 함수
-	}
-	
-	public static void swap(int[] arr,int i,int j) { 
-		int temp=arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
+		int[] tmp = arr.clone();
+		
+		int part1 = lb;
+		int part2 = middle + 1;
+		int index = lb;
+		
+		while(part1 <= middle && part2 <= ub) {
+			if(tmp[part1] <= tmp[part2]) {
+				arr[index++] = tmp[part1++];
+			} 
+			else {
+				arr[index++] = tmp[part2++];
+			}
+		}
+		
+		for(int i=0; i<=middle-part1; i++) {
+			arr[index+i] = tmp[part1+i];
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -34,5 +45,12 @@ public class MergeSort {
 			arr[i]=Integer.parseInt(br.readLine());
 		}
 		mergeSort(arr,0,N-1);
+		
+		StringBuffer sb = new StringBuffer();
+
+		for(int i=0;i<N;i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		System.out.println(sb);
 	}
 }
