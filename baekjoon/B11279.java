b8be135afcc4270b7e57d8646c7152ca95f50957
LeeHuiJu <leemoon63@gmail.com>
2022-07-08 23:48:48 +0900

    최대힙 출력오류 수정

--- baekjoon/B11279.java
+++ baekjoon/B11279.java
@@ -1,3 +1,4 @@
+//https://www.acmicpc.net/problem/11279
 package baekjoon;
 
 import java.io.BufferedReader;
@@ -9,19 +10,26 @@ import java.util.PriorityQueue;
 public class B11279 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        PriorityQueue<Integer> que = new PriorityQueue<>(Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-
-            if (num == 0)
-                sb.append(que.poll()).append('\n');
-            else 
-            	que.add(num);
-        }
-        System.out.println(sb.toString());
+        int N = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		
+        while(N-->0) {
+			int num=Integer.parseInt(br.readLine());
+			
+			if(num==0) {
+				if(queue.isEmpty()) 
+					sb.append("0").append("\n");
+				else
+					sb.append(queue.poll()).append("\n");
+			}
+				
+			else {
+				queue.add(num);
+			}
+		}
+		System.out.println(sb);
 	}
 }
