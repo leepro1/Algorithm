077f8fc6d5d938fbd23ac078626e737784c562ba
LeeHuiJu <leemoon63@gmail.com>
2022-07-10 17:00:49 +0900

    N괴 M

---
+++ baekjoon/B15649.java
@@ -0,0 +1,22 @@
+//https://www.acmicpc.net/problem/15649
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15649 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		StringBuilder sb=new StringBuilder();
+		
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		
+		for(int i=1;i<=N;i++) {
+			
+		}
+	}
+}
