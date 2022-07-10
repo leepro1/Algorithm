7856cc35acf62f191e070d7aebb5b2f8f390931b
LeeHuiJu <leemoon63@gmail.com>
2022-07-10 23:59:32 +0900

    N,M 그래프 수열

--- baekjoon/B15649.java
+++ baekjoon/B15649.java
@@ -7,16 +7,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B15649 {
+	public static int[] arr;
+	public static boolean[] visited;
+	public static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st=new StringTokenizer(br.readLine());
-		StringBuilder sb=new StringBuilder();
 		
 		int N=Integer.parseInt(st.nextToken());
 		int M=Integer.parseInt(st.nextToken());
 		
-		for(int i=1;i<=N;i++) {
-			
+		arr = new int[M];
+		visited = new boolean[N];
+		dfs(N, M, 0);
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int N, int M, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i + 1;
+				dfs(N, M, depth + 1);
+				visited[i] = false;
+			}
 		}
 	}
 }
