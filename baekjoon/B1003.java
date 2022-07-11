7427651a488591d74e232620a3b53806ebc0d48f
LeeHuiJu <leemoon63@gmail.com>
2022-07-11 23:59:21 +0900

    피보나치 dp풀이 수정

--- baekjoon/B1003.java
+++ baekjoon/B1003.java
@@ -1,37 +1,36 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B1003 {
-	static int[][] cnt=new int[41][2];
+	public static Integer[][] dp=new Integer[41][2];
 	
-	private static int[] fibonacci(int n) {
-		if(n==0) {
-			cnt[0][0]=1;
-			cnt[0][1]=0;
-			return cnt[0];
-		}
-		if(n==1) {
-			cnt[1][0]=0;
-			cnt[1][1]=1;
-			return cnt[1];
-		}
-		if(cnt[n][0]!=0||cnt[n][1]!=0) {
-			return cnt[n];
-		}
-		cnt[n][0]+=fibonacci(n-1)[0]+fibonacci(n-2)[0];
-		cnt[n][1]+=fibonacci(n-1)[1]+fibonacci(n-2)[1];
-		return cnt[n];
-	}
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int T=sc.nextInt();
-	
-		for(int i=0;i<T;i++) {
-			int N=sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		
+		int T=Integer.parseInt(br.readLine());
+		
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		dp[1][0] = 0;
+		dp[1][1] = 1;
+		
+		while(T-->0) {
+			int N=Integer.parseInt(br.readLine());
 			fibonacci(N);
-			System.out.println(cnt[N][0]+" "+cnt[N][1]);
+			sb.append(dp[N][0]+" "+dp[N][1]).append('\n');
 		}
-		sc.close();
+	}
+	
+	public static Integer[] fibonacci(int n) {
+		if(dp[n][0]==null || dp[n][1]==null) {
+			dp[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
+			dp[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
+		}
+		
+		return dp[n];
 	}
 }
