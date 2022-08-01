5854934a4ad5fa1b7355d1d506a0bd7c6ced9541
LeeHuiJu <leemoon63@gmail.com>
2022-08-01 23:59:28 +0900

    IOI문제 반례해결

--- baekjoon/B5525.java
+++ baekjoon/B5525.java
@@ -15,29 +15,21 @@ public class B5525 {
 		
 		int answerCnt=0;
 		int nCnt=0;
-		int plusCnt=0;
 		
-		for(int i=0;i<M-1;i++) {
-            if(S.charAt(i)=='I' && S.charAt(i+1)=='O') {
-            	nCnt++;
-                
-                if(nCnt>=N) {
-                    if(S.charAt(i+2)=='I') {
-                    	answerCnt++;
-                    	plusCnt++;
-                    }
-                    else {
-                    	nCnt=0;
-                    	answerCnt+=plusCnt;
-                    	plusCnt=0;
-                    }
-                }
-                i++;
-            }
-            else {
-            	nCnt=0;
-            }
-        }
+		for(int i=1;i<M-1;i++) {
+			if(S.charAt(i-1)=='I' && S.charAt(i)=='O' && S.charAt(i+1)=='I') {
+				nCnt++;
+				
+				if(nCnt==N) {
+					nCnt--;
+					answerCnt++;
+				}
+				i++;
+			}
+			else
+				nCnt=0;
+		}
+		
         System.out.println(answerCnt);
     }
 }
