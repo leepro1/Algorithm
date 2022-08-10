b96a74f5870d3bb1b5d3c54c21dc8c15c7b83368
LeeHuiJu <leemoon63@gmail.com>
2022-08-10 23:59:43 +0900

    배추지렁이_dfs

--- baekjoon/B1012.java
+++ baekjoon/B1012.java
@@ -7,21 +7,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B1012 {
-	public static int[][] matrixGraph;
+	static int[][] matrixGraph;
+	static boolean[][] visited;
+	
+	static int M;
+	static int N;
+	static int K;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
 		int T=Integer.parseInt(br.readLine());
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		
-		int M=Integer.parseInt(st.nextToken());
-		int N=Integer.parseInt(st.nextToken());
-		int K=Integer.parseInt(st.nextToken());
 		
 		while(T-->0) {
-			matrixGraph=new int[M][N];
-			int cnt=0;
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			M=Integer.parseInt(st.nextToken());
+			N=Integer.parseInt(st.nextToken());
+			K=Integer.parseInt(st.nextToken());
+			
+			matrixGraph=new int[N][K];
+			visited=new boolean[N][K];
+			
 			while(K-->0) {
 				st=new StringTokenizer(br.readLine());
 				int x=Integer.parseInt(st.nextToken());
@@ -29,6 +35,18 @@ public class B1012 {
 				addEdge(x,y);
 			}
 			
+			int cnt=0;
+			
+			for(int i=0;i<M;i++) {
+				for(int j=0;j<N;j++) {
+					if(matrixGraph[i][j]==1) {
+						if(matrixGraph[i][j]==1 && !visited[i][j]) {
+							dfs(i,j);
+							cnt++;
+						}
+					}
+				}
+			}
 			System.out.println(cnt);
 		}
 	}
@@ -36,4 +54,16 @@ public class B1012 {
 		matrixGraph[v1][v2]=1;
 	}
 	
+	public static void dfs(int x,int y) {
+		visited[x][y]=true;		
+		
+		if(x-1>=0 && matrixGraph[x-1][y]==1 && !visited[x-1][y])
+			dfs(x-1,y);
+		if(x+1<M && matrixGraph[x+1][y]==1 && !visited[x+1][y])
+			dfs(x+1,y);
+		if(y-1>=0 && matrixGraph[x][y-1]==1 && !visited[x][y-1])
+			dfs(x,y-1);
+		if(y+1<N && matrixGraph[x][y+1]==1 && !visited[x][y+1])
+			dfs(x,y+1);
+	}
 }
