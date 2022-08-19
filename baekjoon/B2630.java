74b0777a07ff616fa39425c429e31ca8b40a0293
LeeHuiJu <leemoon63@gmail.com>
2022-08-19 16:03:59 +0900

    색종이_분할/재귀

--- baekjoon/B2630.java
+++ baekjoon/B2630.java
@@ -1,4 +1,4 @@
-//https://www.acmicpc.net/problem/1780
+//https://www.acmicpc.net/problem/2630
 package baekjoon;
 
 import java.io.BufferedReader;
@@ -6,57 +6,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B1780 {
-	private static int[][] matrix;
-	private static int minusCnt=0;
+public class B2630 {
+	private static boolean[][] matrix; //0은 false, 1은 true
 	private static int zeroCnt=0;
-	private static int plusCnt=0;
-
+	private static int oneCnt=0;
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int N=Integer.parseInt(br.readLine());
-		matrix=new int[N][N];
 		
-		for(int i=0;i<N;i++) {
+		matrix=new boolean[N][N];
+		
+		for (int i=0;i<N;i++) {
 			StringTokenizer st=new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
-				matrix[i][j]=Integer.parseInt(st.nextToken());
+				if(st.nextToken().equals("0"))
+					matrix[i][j]=false;
+				else
+					matrix[i][j]=true;
 			}
 		}
 		divideAndCnt(0,0,N);
 		
-		System.out.println(minusCnt+"\n"+zeroCnt+"\n"+plusCnt);
+		System.out.println(zeroCnt+"\n"+oneCnt);
 	}
 	
 	private static void divideAndCnt(int row,int col,int size) {
 		if(check(row,col,size)) {
-				if(matrix[row][col]==-1)
-					minusCnt++;
-				if(matrix[row][col]==0)
-					zeroCnt++;
-				if(matrix[row][col]==1)	
-					plusCnt++;
-			}
+			if(matrix[row][col]==false)
+				zeroCnt++;
+			else
+				oneCnt++;
+		}
+		
 		else {
-			int cutSize=size/3;
+			int cutSize=size/2;
 			
 			for(int i=row;i<row+size;i+=cutSize) {
-				for(int j=col;j<col+size;j+=cutSize)
+				for(int j=col;j<col+size;j+=cutSize) {
 					divideAndCnt(i,j,cutSize);
+				}
 			}
 		}
 	}
 	
 	private static boolean check(int row,int col,int size) {
-		int tempValue=matrix[row][col];
 		
 		for(int i=row;i<row+size;i++) {
 			for(int j=col;j<col+size;j++) {
-				if(tempValue!=matrix[i][j])
+				if(matrix[i][j]!=matrix[row][col])
 					return false;
 			}
 		}
+		
 		return true;
 	}
 }
