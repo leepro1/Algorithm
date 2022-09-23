e36e13062a222cbe252102db562293386a8234b4
LeeHuiJu <57425121+LeeHuiJu@users.noreply.github.com>
2022-09-23 16:25:55 +0900

    N과M(8)_백트래킹

---
+++ baekjoon/B15657.java
@@ -0,0 +1,51 @@
+//https://www.acmicpc.net/problem/15657
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B15657 {
+	public static int[] arr;
+	public static int[] numArr;
+	public static int N,M;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+		numArr = new int[N];
+		
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			numArr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numArr);
+		
+		dfs(0,0);
+		System.out.println(sb);
+		
+	}
+	public static void dfs(int start,int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val+" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=start;i<N;i++) {
+			arr[depth]=numArr[i];
+			dfs(i, depth+1);
+		}
+	}
+}
