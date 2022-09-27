a5d39394703fb5e0c100c29f9ff8b976fc4bbf25
Lee, HuiJu <leemoon63@gmail.com>
2022-09-27 23:42:32 +0900

    N과M(9)_백트래킹

---
+++ baekjoon/B15663.java
@@ -0,0 +1,61 @@
+//https://www.acmicpc.net/problem/15663
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B15663 {
+	public static int[] arr;
+	public static int[] numArr;
+	public static boolean[] visited;
+	public static int N,M;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		numArr = new int[N];
+		visited = new boolean[N];
+		
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			numArr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numArr);
+		
+		dfs(0,0);
+		System.out.println(sb);
+		
+	}
+	
+	public static void dfs(int start,int depth) {
+		int duplicate=0;
+		
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val+" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(!visited[i] && duplicate!=numArr[i]) {
+				visited[i]=true;
+				duplicate=numArr[i];
+				arr[depth]=numArr[i];
+				dfs(i+1, depth+1);
+				visited[i]=false;
+			}
+		}
+	}
+}
