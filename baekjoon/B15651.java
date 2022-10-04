725fab27616054f3d0fc0a0cacb72350cae585f7
Lee, HuiJu <leemoon63@gmail.com>
2022-10-04 23:45:49 +0900

    N과M(3)_백트래킹

---
+++ baekjoon/B15651.java
@@ -0,0 +1,40 @@
+//https://www.acmicpc.net/problem/15651
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15651 {
+	public static int[] arr;
+	public static int N,M;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+		dfs(1,0);
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int start,int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val+" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=1;i<=N;i++) {
+			arr[depth]=i;
+			dfs(i, depth+1);
+		}
+	}
+}
