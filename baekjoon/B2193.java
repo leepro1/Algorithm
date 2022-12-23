7592115b9b8211f6429e6a1ef06bcb9d595f2325
Lee, HuiJu <leemoon63@gmail.com>
2022-12-23 18:11:02 +0900

    이친수_dp

---
+++ baekjoon/B2193.java
@@ -0,0 +1,24 @@
+//https://www.acmicpc.net/problem/2193
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2193 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		long dp[] = new long[n + 1];
+
+		dp[0] = 0;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		System.out.println(dp[n]);
+	}
+}
