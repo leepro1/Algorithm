1b5aff41f193242e707ad4db3badfeba17ff7056
Lee, HuiJu <leemoon63@gmail.com>
2022-12-28 21:31:42 +0900

    쇠막대자르기_스택

---
+++ baekjoon/B10799.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+public class B10799 {
+	public static void main(String[] args)  throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str=br.readLine();
+		
+		Stack<Integer> stack = new Stack<>();
+
+		int index=1;
+		int cnt=0;
+		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)=='(') {
+				stack.push(index);
+			}
+			else { // ')' 
+				if(index-stack.peek()==1) {
+					stack.pop();
+					cnt+=stack.size();
+				}
+				else {
+					stack.pop();
+					cnt+=1;
+				}
+				
+			}
+			
+			index++;
+		}
+		System.out.println(cnt);
+	}
+}
