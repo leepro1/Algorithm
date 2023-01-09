89eb5df15d72fd601cb6415804e03afaf40087de
Lee, HuiJu <leemoon63@gmail.com>
2023-01-09 23:59:37 +0900

    포도주 시식_dp

---
+++ baekjoon/B2156.java
@@ -0,0 +1,40 @@
+//https://www.acmicpc.net/problem/2156
+//Top-down방식 풀이
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2156 {
+	private static int[] arr;
+	private static int[] dp;
+	private static int n;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+
+		arr = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		dp = new int[n + 1];
+		dp[1] = arr[1];
+		if (n != 1) {
+			dp[2] = arr[1] + arr[2];
+		}
+
+		System.out.println(recur(n));
+	}
+
+	private static int recur(int N) {
+		if (dp[N]==0) {
+			dp[N] = Math.max(Math.max(recur(N - 2), recur(N - 3) + arr[N - 1]) + arr[N], recur(N - 1));
+		}
+		return dp[N];
+	}
+}
---
+++ baekjoon/B2156_2.java
@@ -0,0 +1,33 @@
+//https://www.acmicpc.net/problem/2156
+//Bottom-up방식 풀이
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2156_2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int[] dp = new int[n + 1];
+		dp[1] = arr[1];
+		if (n != 1) {
+			dp[2] = arr[1] + arr[2];
+		}
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+
+		}
+		System.out.println(dp[n]);
+	}
+}
