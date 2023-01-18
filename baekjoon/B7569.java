4b70bc220d0369fc5a1367c2f6b0af539a11fae3
Lee, HuiJu <leemoon63@gmail.com>
2023-01-18 18:07:41 +0900

    토마토2_BFS

---
+++ baekjoon/B7569.java
@@ -0,0 +1,82 @@
+//https://www.acmicpc.net/problem/7579
+/*
+ 토마토문제2 - BFS 
+ Queue를 사용하여 BFS로 푼다. 이 때 상하좌우를 검사한다. 토마토가 있는 모든 정점을 돌아가면서 탐색한다.
+ 토마토가 있는 모든 정점을 돌아가면서 하는 것 구현 -> 처음에 Queue에 넣어 풀어보자.
+ 거리가 늘어날 때마다 ++를 해주며 계산하자.
+*/
+
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B7569 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+
+		int[][][] visited = new int[H][N][M];
+
+		Queue<int[]> queue = new LinkedList<int[]>(); // bfs로 풀기 위한 queue
+
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < N; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < M; k++) {
+					visited[i][j][k] = Integer.parseInt(st.nextToken());
+					if (visited[i][j][k] == 1) {
+						queue.offer(new int[] { i, j, k });
+					}
+				}
+			}
+		}
+
+		int[] dx = { -1, 1, 0, 0, 0, 0 }; // 상하좌우위아래 검사를 위한 배열
+		int[] dy = { 0, 0, -1, 1, 0, 0 }; // 상하좌우위아래 검사를 위한 배열
+		int[] dz = { 0, 0, 0, 0, -1, 1 }; // 상하좌우위아래 검사를 위한 배열
+
+		while (!queue.isEmpty()) {
+			int[] temp = queue.poll();
+
+			for (int i = 0; i < 6; i++) {
+				int nextX = temp[1] + dx[i];
+				int nextY = temp[2] + dy[i];
+				int nextZ = temp[0] + dz[i];
+
+				if (nextX < 0 || nextY < 0 || nextZ < 0 || nextX >= N || nextY >= M || nextZ >= H)
+					continue;
+				if (visited[nextZ][nextX][nextY] == 0) {
+					visited[nextZ][nextX][nextY] = visited[temp[0]][temp[1]][temp[2]] + 1;
+					queue.offer(new int[] { nextZ, nextX, nextY });
+				}
+			}
+		}
+
+		int cnt = 0;
+
+		Loop: 
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < M; k++) {
+					if (visited[i][j][k] == 0) { // 안익은 토마토가 있으면 0으로 break
+						cnt = 0;
+						break Loop;
+					}
+					if (visited[i][j][k] > cnt) {
+						cnt = visited[i][j][k];
+					}
+				}
+			}
+		}
+		System.out.println(cnt - 1); // 전부 익은상태는 1->0 으로, 나머지(안익은 토마토가 있는 상태 포함)는 -1씩을 해주어야함
+	}
+}
