414bb3a76f8c58f5c0268b38905cf2c10dcca7d2
Lee, HuiJu <leemoon63@gmail.com>
2023-02-22 17:47:14 +0900

    퇴사, 포도주시식_dp(수정)

--- baekjoon/B14501.java
+++ baekjoon/B14501.java
@@ -8,26 +8,26 @@ import java.util.StringTokenizer;
 
 public class B14501 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-		int N=Integer.parseInt(br.readLine());
-		int[] T=new int[N+1];
-		int[] P=new int[N+1];
-		int[] dp = new int[N+1];
-		
-		for(int i=0;i<N;i++) {
-			StringTokenizer st=new StringTokenizer(br.readLine());
-			T[i]=Integer.parseInt(st.nextToken());
-			P[i]=Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] T = new int[N];
+		int[] P = new int[N];
+		int[] dp = new int[N + 1];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		for(int i=1;i<=N;i++) {
-			if(N<i+T[i]) //퇴사일을 넘겼을 때
-				dp[i]=dp[i-1];
-			else
-				dp[i]=Math.max(dp[i-1],P[i]+dp[i+T[i]]);
+
+		for (int i = 0; i < N; i++) {
+			if (N >= i + T[i])
+				dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+
+			dp[i + 1] = Math.max(dp[i], dp[i + 1]);
 		}
-		
+
 		System.out.println(dp[N]);
 	}
 }
--- baekjoon/B2156.java
+++ baekjoon/B2156.java
@@ -1,4 +1,4 @@
-//https://www.acmicpc.net/problem/2156
+//https://www.acmicpc.net/problem/2156..
 //Top-down방식 풀이
 package baekjoon;
 
@@ -8,13 +8,12 @@ import java.io.InputStreamReader;
 
 public class B2156 {
 	private static int[] arr;
-	private static int[] dp;
-	private static int n;
+	private static Integer[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
 		arr = new int[n + 1];
 
@@ -22,7 +21,8 @@ public class B2156 {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		dp = new int[n + 1];
+		dp = new Integer[n + 1];
+		dp[0] = 0;
 		dp[1] = arr[1];
 		if (n != 1) {
 			dp[2] = arr[1] + arr[2];
@@ -32,7 +32,7 @@ public class B2156 {
 	}
 
 	private static int recur(int N) {
-		if (dp[N]==0) {
+		if (dp[N] == null) {
 			dp[N] = Math.max(Math.max(recur(N - 2), recur(N - 3) + arr[N - 1]) + arr[N], recur(N - 1));
 		}
 		return dp[N];
