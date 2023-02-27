d84f58971f0b37d47b5521bd19bc1eb290d6065e
Lee, HuiJu <leemoon63@gmail.com>
2023-02-27 23:45:26 +0900

    플로이드_INF 수정

--- baekjoon/B11404.java
+++ baekjoon/B11404.java
@@ -1,9 +1,10 @@
-//https://www.acmicpc.net/problem/1389
+//https://www.acmicpc.net/problem/11404
 /*
- * 케빈 베이컨의 6단계 법칙 - 그래프, 플로이드 워셜
+ * 플로이드 - 플로이드 워셜
  * 플로이드 워셜 알고리즘은 dp를 기반으로 인접행렬을 이용하여 사용하는 방식이다.
  * 하나의 정점이 아닌 모든 정점에서의 최단거리를 구할 수 있다.
  * Integer.MAX_VALUE는 거리를 갱신할 때 오버플로우가 난다.
+ * 그리고 거리는 n(100) x m(100000)은 넘어주어야 최악의 경우도 처리할 수 있다.
  */
 package baekjoon;
 
@@ -12,29 +13,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B1389 {
+public class B11404 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(st.nextToken()); // 정점
-		int m = Integer.parseInt(st.nextToken()); // 간선
+		int n = Integer.parseInt(br.readLine()); // 정점
+		int m = Integer.parseInt(br.readLine()); // 간선
 
 		int[][] dist = new int[n + 1][n + 1]; // 거리테이블
 
 		// 거리 초기화
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				dist[i][j] = (i == j) ? 0 : 50001;
+				dist[i][j] = (i == j) ? 0 : 10000001;
 			}
 		}
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()); // 정점
-			int y = Integer.parseInt(st.nextToken()); // 간선
 
-			dist[x][y] = 1;
-			dist[y][x] = 1;
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()); // 정점
+			int b = Integer.parseInt(st.nextToken()); // 간선
+			int c = Integer.parseInt(st.nextToken()); // 가중치
+
+			dist[a][b] = Math.min(dist[a][b], c); // 경로가 하나가 아니다.
 		}
 
 		// 최단 거리 갱신
@@ -46,27 +47,15 @@ public class B1389 {
 			}
 		}
 
-		int[] kevinNum = new int[n + 1];
-		int min = 50001;
-
+		// 출력
 		for (int i = 1; i <= n; i++) {
-			int sum = 0;
 			for (int j = 1; j <= n; j++) {
-				sum += dist[i][j];
+				if (dist[i][j] == 10000001) // 경로가 없을 때
+					System.out.print(0 + " ");
+				else
+					System.out.print(dist[i][j] + " ");
 			}
-
-			kevinNum[i] = sum;
-
-			if (sum < min) {
-				min = sum;
-			}
-		}
-
-		for (int i = 1; i <= n; i++) {
-			if (kevinNum[i] == min) {
-				System.out.println(i);
-				return;
-			}
+			System.out.println();
 		}
 
 	}
