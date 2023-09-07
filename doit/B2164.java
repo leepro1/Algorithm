6f912352c701af81bf894f5606520bc9ff64c8b6
Lee, HuiJu <leemoon63@gmail.com>
2023-09-08 00:29:17 +0900

    13_카드게임

---
+++ doit/B2164.java
@@ -0,0 +1,31 @@
+/*
+ * https://www.acmicpc.net/problem/2164
+ * 카드2
+ * 앞에서 remove한 것을 뒤에서 add하기에 큐를 이용하자. 
+ */
+package doit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class B2164 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 1; i <= N; i++) {
+			queue.offer(i);
+		}
+
+		while (queue.size() > 1) {
+			queue.poll();
+			queue.offer(queue.poll());
+		}
+
+		System.out.println(queue.peek());
+	}
+}
