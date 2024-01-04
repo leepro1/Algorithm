1606da91c838e8a357ed1d0b6ceb60655839d396
Huiju Lee <leemoon63@gmail.com>
2024-01-04 23:31:47 +0900

    2817_부분 수열의 합

--- swea/S2817.java
+++ swea/S2817.java
@@ -3,63 +3,47 @@ package swea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class S2817 {
+	static int[] arr;
+	static int n, k, cnt;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
-		int n, k, cnt;
-		int start, end;
-		int[] arr, cntArr;
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			k = Integer.parseInt(st.nextToken());
-			arr = new int[n + 1];
-			cntArr = new int[101];
+			arr = new int[n];
 
 			st = new StringTokenizer(br.readLine());
-			int input;
-			for (int i = 1; i <= n; i++) {
-				input =Integer.parseInt(st.nextToken());
-				if(cntArr[input]==0) {
-					arr[i] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			Arrays.sort(arr);
-
-		/*	for (int i = 1; i <= n; i++) {
-				sum[i] = sum[i - 1] + arr[i];
-			}
-
 			cnt = 0;
-			start = 1;
-			end = 2;
-			while (true) {
-				nowSum = sum[end] - sum[start - 1];
-				System.out.println(nowSum);
-				if (nowSum == k) {
-					System.out.println(start+"더한다"+end);
-					cnt++;
-					start++;
-					end++;
-				} else if (nowSum < k) {
-					end++;
-				} else if (nowSum > k) {
-					start++;
-				}
 
-				if (start > n || end > n) {
-					break;
-				}
-*/			}
+			dfs(0, 0);
 
-			sb.append("#" + t + " "  + "\n");
+			sb.append("#" + t + " " + cnt + "\n");
 		}
 		System.out.println(sb);
+	}
+
+	static void dfs(int start, int sum) {
+		if (start == n) {
+			if (sum == k) {
+				cnt++;	
+			}
+			return;
+		}
+
+		dfs(start + 1, sum);
+		sum += arr[start];
+		dfs(start + 1, sum);
 	}
 }
