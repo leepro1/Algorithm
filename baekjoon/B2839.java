0efca25dd56aea2d9e8eda1254e94826667ec8ce
Huiju Lee <leemoon63@gmail.com>
2024-02-13 19:53:54 +0900

    설탕배달_그리디

--- baekjoon/B2839.java
+++ baekjoon/B2839.java
@@ -1,26 +1,27 @@
 package baekjoon;
 
-import java.util.Scanner;
+//수행시간:120ms       메모리:14,228kb
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B2839 {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int N=sc.nextInt();
-		int box=0;
-		
-		while(true) {
-			if(N%5==0) {
-				System.out.println(N/5+box);
-				break;
-			}
-			else if(N<3) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int box = 0;
+
+		while (true) {
+			if (N != 0 && N < 3) {
 				System.out.println(-1);
 				break;
+			} else if (N % 5 == 0) {
+				System.out.println(N / 5 + box);
+				break;
 			}
-			N-=3;
+			N -= 3;
 			box++;
 		}
-		
-		sc.close();
+
 	}
 }
