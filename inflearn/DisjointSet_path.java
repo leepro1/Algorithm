29975d4afcae93536f0ba36c5eba0ae42857f5d9
Huiju Lee <leemoon63@gmail.com>
2024-03-18 22:58:51 +0900

    DisjointSet_path 최적화

---
+++ inflearn/DisjointSet_path.java
@@ -0,0 +1,49 @@
+package inflearn;
+
+import java.util.Arrays;
+
+public class DisjointSet_path {
+	static int N;
+	static int[] parent;
+
+	public static void makeSet() {
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+	}
+
+	public static int find(int x) {
+		if (x == parent[x])
+			return x;
+
+		return parent[x] = find(parent[x]);
+	}
+
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+
+		if (x <= y)
+			parent[y] = x;
+		else
+			parent[x] = y;
+	}
+
+	public static void main(String[] args) {
+		N = 6;
+		makeSet();
+		System.out.println(Arrays.toString(parent));
+
+		union(1, 2);
+		System.out.println(Arrays.toString(parent));
+		union(3, 4);
+		System.out.println(Arrays.toString(parent));
+		union(1, 5);
+		System.out.println(Arrays.toString(parent));
+		union(3, 5);
+		System.out.println(Arrays.toString(parents));
+		find(4);
+		System.out.println(Arrays.toString(parent));
+	}
+}
