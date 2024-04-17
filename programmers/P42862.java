029ada4c8b9e86c6f94d9554ca119155b83d03c9
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-04-17 17:08:18 +0900

    체육복_그리디

---
+++ programmers/P42862.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class P42862 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n-lost.length;
+        
+        Arrays.sort(reserve);
+        
+        boolean[] check = new boolean[n+2];
+        for(int i=0; i<lost.length; i++){
+            check[lost[i]] = true;
+        }
+        
+        int loseCnt = 0;
+        for(int i=0; i<reserve.length; i++){
+            while(loseCnt < lost.length - 1 && lost[loseCnt]<reserve[i]){
+                loseCnt++;
+            }
+            
+            if(check[reserve[i]]){
+                check[reserve[i]] = false;
+                answer++;
+            } else if(lost[loseCnt] !=reserve[i] && check[reserve[i] - 1]){
+                check[reserve[i] - 1] = false;
+                answer++;
+            } else if(lost[loseCnt] !=reserve[i] && check[reserve[i] + 1]){
+                check[reserve[i] + 1] = false;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
