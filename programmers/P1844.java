2d9c21c35bdd684d5b620335e6c9f7be20f16333
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-05-11 23:58:03 +0900

    게임 맵 최단거리_BFS

---
+++ programmers/P1844.java
@@ -0,0 +1,37 @@
+pacakage programmers;
+
+import java.util.*;
+
+class P1844 {
+    public int solution(int[][] maps) {
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] {0, 0});
+        maps[0][0] = 1;
+        
+        while(!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = temp[0] + dx[i];
+                int ny = temp[1] + dy[i];
+                
+                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length)
+                    continue;
+                
+                if(nx == maps.length - 1  && ny == maps[0].length - 1) {
+                    return maps[temp[0]][temp[1]];
+                }
+                    
+                if(maps[nx][ny] == 0) {
+                    maps[nx][ny] = maps[temp[0]][temp[1]] + 1;
+                    queue.add(new int[] {nx, ny});
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
