baeedc8199ce066bbd55113a754b73ee1017d117
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-06-02 17:31:18 +0900

    조건에 맞는 사원 정보 조회하기 수정_JOIN, GROUP

--- programmers/P284527.sql
+++ programmers/P284527.sql
@@ -1,8 +1,22 @@
 SELECT 
-    SUM(g.SCORE) AS SCORE, g.EMP_NO, EMP_NAME, POSITION, EMAIL
+    g.SCORE,
+    e.EMP_NO, 
+    e.EMP_NAME, 
+    e.POSITION, 
+    e.EMAIL
 FROM 
-    HR_GRADE g, HR_DEPARTMENT d, HR_EMPLOYEES e
-GROUP BY
-    g.EMP_NO
-ORDER BY
-    SCORE DESC;
+    HR_EMPLOYEES e
+JOIN 
+    (SELECT 
+         EMP_NO, SUM(SCORE) AS SCORE
+     FROM 
+         HR_GRADE
+     WHERE
+        YEAR = 2022
+     GROUP BY 
+         EMP_NO
+     ORDER BY 
+         SCORE DESC
+     LIMIT 1) g
+ON 
+    e.EMP_NO = g.EMP_NO;
