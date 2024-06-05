e64ff087f1137d855ee2547d14b75458edff7f32
Huiju <leemoon63@gmail.com>
2024-06-05 23:39:57 +0900

    전화번호_문자열

--- programmers/P42577.java
+++ programmers/P42577.java
@@ -1,16 +1,14 @@
-class Solution {
+package programmers;
+
+import java.util.Arrays;
+
+public class P42577 {
     public boolean solution(String[] phone_book) {
-        for(int i = 0; i < phone_book.length; i++){
-            for(int j = i + 1; j < phone_book.length; j++){
-                
-                if(phone_book[i].length() < phone_book[j].length()) {
-                    if(phone_book[i].indexOf(phone_book[j]) != -1)
-                        return false;
-                } else {
-                    if(phone_book[j].indexOf(phone_book[i]) != -1)
-                        return false;
-                }
-            }
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].length() > phone_book[i].length() && phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
         }
         return true;
     }
