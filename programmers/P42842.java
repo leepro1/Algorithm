08a8f73adfa936c3d054d33fe771cd2a1883262f
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-06-10 23:40:41 +0900

    카펫_완전탐색

---
+++ programmers/P42842.java
@@ -0,0 +1,24 @@
+pacakage programmers;
+
+class P42842 {
+    public int[] solution(int brown, int yellow) {
+        
+        int width = 0;
+        int length = 0;
+        int limit = brown + yellow;
+        
+        for(int i = 1; i <= Math.sqrt(limit); i++) {
+            
+            if(limit % i == 0) {
+                width = i;
+                length = limit / width;
+                
+                if(2 * width + 2 * (length - 2) == brown)
+                    break;
+            }
+        }
+        
+        int[] answer = new int[] {length, width};
+        return answer;
+    }
+}
