c6573642484cd5bf2c056cd79992a05603306310
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-07-10 09:49:18 +0900

    업그레이드 할 수 없는 아이템 구하기_NOT EXISTS

---
+++ programmers/P273712.sql
@@ -0,0 +1,15 @@
+SELECT
+    ITEM_ID, ITEM_NAME, RARITY
+FROM
+    ITEM_INFO i
+WHERE
+    NOT EXISTS (
+        SELECT DISTINCT
+            ITEM_ID
+        FROM
+            ITEM_TREE
+        WHERE
+            i.ITEM_ID = PARENT_ITEM_ID
+    )
+ORDER BY
+    ITEM_ID DESC;
