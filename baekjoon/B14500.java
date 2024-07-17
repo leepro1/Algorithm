d881d06e851940bf84b9774c8736e38f445bb3d9
HuiJu <leemoon63@gmail.com>
2024-07-17 23:11:17 +0900

    테트로미노_백트래킹

---
+++ baekjoon/B14500.java
@@ -0,0 +1,71 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B14500 {
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, 1, map[i][j]);
+                visited[i][j] = false;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int x, int y, int depth, int sum) {
+        if (depth == 4) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny])
+                continue;
+
+            if (depth == 2) {
+                visited[nx][ny] = true;
+                dfs(x, y, depth + 1, sum + map[nx][ny]);
+                visited[nx][ny] = false;
+            }
+
+            visited[nx][ny] = true;
+            dfs(nx, ny, depth + 1, sum + map[nx][ny]);
+            visited[nx][ny] = false;
+
+        }
+
+    }
+}
