7ad1a28e96316cf8973b00eef84d841839af450e
leepro1 <57425121+leepro1@users.noreply.github.com>
2024-09-27 22:02:55 +0900

    특정 조건을 만족하는 물고기별 수와 최대 길이 구하기_sub

---
+++ programmers/P298519_2.sql
@@ -0,0 +1,17 @@
+SELECT
+    COUNT(*) AS FISH_COUNT, 
+    MAX(LENGTH) AS MAX_LENGTH,
+    FISH_TYPE
+FROM
+    (
+        SELECT 
+            ID, FISH_TYPE, IFNULL(LENGTH, 10) AS LENGTH
+        FROM
+            FISH_INFO 
+    ) i
+GROUP BY
+    FISH_TYPE
+HAVING
+    AVG(LENGTH) >= 33
+ORDER BY
+    FISH_TYPE;
