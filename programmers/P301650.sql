e88f98568dca5604337656c82058958ae425a011
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-10-07 09:10:23 +0900

    특정 세대의 대장균 찾기_IN

---
+++ programmers/P301650.sql
@@ -0,0 +1,22 @@
+SELECT
+    ID
+FROM
+    ECOLI_DATA
+WHERE
+    PARENT_ID IN (
+        SELECT 
+            ID
+        FROM
+            ECOLI_DATA
+        WHERE
+            PARENT_ID IN (
+                SELECT 
+                    ID
+                FROM
+                    ECOLI_DATA
+                WHERE
+                    PARENT_ID IS NULL
+            )
+    )
+ORDER BY
+    ID;
