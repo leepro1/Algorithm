ea5072d7bd5de2089e27f23da6ad257e7bbce221
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-10-12 19:16:11 +0900

    Create L1581.sql

---
+++ leetcode/L1581.sql
@@ -0,0 +1,13 @@
+SELECT
+    customer_id,
+    COUNT(*) AS count_no_trans
+FROM
+    Visits
+LEFT JOIN
+    Transactions
+USING
+    (visit_id)
+WHERE
+    transaction_id IS NULL
+GROUP BY
+    customer_id;
