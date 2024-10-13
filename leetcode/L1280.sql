b025476aa9f2f9f6a53a680af91bfd7cf770438c
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-10-14 00:09:10 +0900

    Update L1280.sql

--- leetcode/L1280.sql
+++ leetcode/L1280.sql
@@ -1,8 +1,18 @@
-SELECT
-    class
-FROM
-    Courses
-GROUP BY
-    class
-HAVING
-    COUNT(*) >= 5;
+SELECT 
+    st.student_id, 
+    st.student_name, 
+    su.subject_name, 
+    COUNT(ex.subject_name) AS attended_exams
+FROM 
+    Students st
+CROSS JOIN 
+    Subjects su
+LEFT JOIN 
+    Examinations ex 
+ON 
+    st.student_id = ex.student_id 
+    AND su.subject_name = ex.subject_name
+GROUP BY 
+    student_id, student_name, subject_name
+ORDER BY 
+    student_id, subject_name;
